f9246bcb592107e793821691bcf35e9d7563dd91
Adriel Rodrigues <120425687+NokFromDFuture@users.noreply.github.com>
2023-10-06 17:39:40 -0300

    index.rb

--- index.rb
+++ index.rb
@@ -1,51 +1,39 @@
+#Crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, depois utilize 
+#uma estrutura de decisão para apresentar alguma das mensagens abaixo:
+#Se XP for menor do que 1.000 = Ferro Se XP for entre 1.001 e 2.000 = Bronze Se XP for entre 2.001 e 5.000 =
+#Prata Se XP for entre 6.001 e 7.000 = Ouro Se XP for entre 7.001 e 8.000 = Platina Se XP for entre 8.001 e 9.000 =
+#Ascendente Se XP for entre 9.001 e 10.000= Imortal Se XP for maior ou igual a 10.001 = Radiante
+#Ao final deve se exibir uma mensagem: "O Herói de nome {nome} está no nível de {nivel}"
+
+
 print "Digite seu nome de heroi: "
 nome = gets.chomp
-
 
 print "Digite a sua quantidade de xp entre 1 a 10.000: "
 experiencia = gets.chomp.to_i
 
 case experiencia
 
-when 0..1000
+    when 0..1000
     experiencia = "Ferro"
 
-when 2001..5000
+    when 2001..5000
     experiencia = "Bronze"
 
-when 5001..7000
+    when 5001..7000
     experiencia = "Prata"
 
-when 7001..8000
+    when 7001..8000
     experiencia = "Ouro"
 
-when 8001..9000
+    when 8001..9000
     experiencia = "Platina"
 
-when 9001..1000
+    when 9001..1000
     experiencia = "Imortal"
   
-else
+    else
     experiencia = "Radiante"
-  
 end
-
-
 puts "O Herói de nome #{nome} está no nível de #{experiencia}"
-
-
-#Instruções do Desafio
-#O Que deve ser utilizado
-#Variáveis
-#Operadores
-#Laços de repetição
-#Estruturas de decisões
-#Objetivo
-#Crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, depois utilize 
-#uma estrutura de decisão para apresentar alguma das mensagens abaixo:
-#Se XP for menor do que 1.000 = Ferro Se XP for entre 1.001 e 2.000 = Bronze Se XP for entre 2.001 e 5.000 =
-#Prata Se XP for entre 6.001 e 7.000 = Ouro Se XP for entre 7.001 e 8.000 = Platina Se XP for entre 8.001 e 9.000 =
-#Ascendente Se XP for entre 9.001 e 10.000= Imortal Se XP for maior ou igual a 10.001 = Radiante
-#Saída
-#Ao final deve se exibir uma mensagem: "O Herói de nome {nome} está no nível de {nivel}"
 
